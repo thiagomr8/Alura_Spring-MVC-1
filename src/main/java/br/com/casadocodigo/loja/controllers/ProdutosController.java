c2b471fa5c185dda8673aa7bd63339b9e1184cf9
Thiago Moreira Ribeiro <thiago.moreira.ribeiro@gmail.com>
2020-04-03 13:57:09 -0300

    Aula 06

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,15 +2,22 @@ package br.com.casadocodigo.loja.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
 @RequestMapping("produtos")
@@ -29,11 +36,19 @@ public class ProdutosController {
     private ProdutoDAO produtoDao;
 
 	@RequestMapping(method=RequestMethod.POST)
-    public String gravar(Produto produto){
-        System.out.println(produto);
-        produtoDao.gravar(produto);
-        return "/produtos/ok";
-    }
+	public ModelAndView gravar(@Valid Produto produto, BindingResult result,  RedirectAttributes redirectAttributes){
+	    if(result.hasErrors()){
+	        return form();
+	    }
+	    produtoDao.gravar(produto);
+	    redirectAttributes.addFlashAttribute("message","Produto cadastrado com sucesso");
+	    return new ModelAndView("redirect:produtos");
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+	    binder.addValidators(new ProdutoValidation());
+	}
     
 	@RequestMapping(method=RequestMethod.GET)
     public ModelAndView listar(){
